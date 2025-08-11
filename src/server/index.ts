d81605c620ede1d640be9d2260f4587f85f37c5a
migayalach <ayalachavezmiguel@gmail.com>
2025-08-11 11:42:57 -0400

    created server

---
+++ src/server/index.ts
@@ -0,0 +1,37 @@
+import express, { Express, Request, Response } from "express";
+import swaggerUi, { serve } from "swagger-ui-express";
+import cors from "cors";
+import helmet from "helmet";
+import rootRouter from "../routes";
+import mongoose from "mongoose";
+
+const server: Express = express();
+
+server.use(
+  "/docs",
+  swaggerUi.serve,
+  swaggerUi.setup(undefined, {
+    swaggerOptions: {
+      url: "/swagger.json",
+      exporer: true,
+    },
+  })
+);
+
+server.use("/api", rootRouter);
+server.use(express.static("public"));
+
+// TODO Mongose connection
+// mongoose.connect("")
+
+server.use(helmet());
+server.use(cors());
+
+server.use(express.urlencoded({ extended: true, limit: "50mb" }));
+server.use(express.json({ limit: "50mb" }));
+
+server.get("/", (request: Request, response: Response) => {
+  response.redirect("/api");
+});
+
+export default server;
